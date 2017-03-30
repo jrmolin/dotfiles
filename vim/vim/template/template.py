bf53c6baf33943fc99847bafc945b8c4008af0e4
mo <jmolinatto@endgame.com>
2017-03-30 14:58:59 -0400

    long-overdue updating

--- vim/vim/template/template.py
+++ vim/vim/template/template.py
@@ -1,24 +1,11 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# generally need this
 import sys
 
-def handle_args(argv):
-	import argparse
-	argparser = argparse.ArgumentParser(description="TODO")
-	argparser.add_argument('-v', '--verbose', dest='verbosity', action='count',
-		default=0, help='verbose mode (aggregate)')
-	argparser.add_argument('query', nargs='*', default=None)
-
-	args = argparser.parse_args(argv)
-
-	return args
-
-def main(argv):
-	args = handle_args(argv)
-
-	return 0
-
+def main(args):
+    result = 0
+    return result
 
 if __name__ == '__main__':
-	sys.exit(main(sys.argv))
+    sys.exit(main(sys.argv[1:]))
