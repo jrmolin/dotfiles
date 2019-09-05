78a8946989f489629ae6cff3797d3bdd89729e0d
mo <jmolinatto@gmail.com>
2019-09-04 20:23:18 -0400

    stash

--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+
+set -e
+
 DEBUG=1
+
+oops() {
+    echo "$0:" "$@" >&2
+    exit 1
+}
+
+require_util() {
+    type "$1" > /dev/null 2>&1 || command -v "$1" > /dev/null 2>&1 |
+        oops "don't have '$1' installed, which we need to $2"
+}
 
 getsystem() {
     local _system="debian"
@@ -71,53 +84,29 @@ install_sqlite() {
     fi
 }
 
-# install vim stuff
-install_vim() {
-    local VIM=$(which vim)
-
-    if [ "x$VIM" = "x" ]; then
-        doit sudo $INSTALL vim
-    fi
-}
-
-# install tmux
-install_tmux() {
-    local TMUX=$(which tmux)
-
-    if [ "x$TMUX" = "x" ]; then
-        doit sudo $INSTALL tmux
-    fi
-}
-
 # install curl
 install_curl() {
-    local CURL=$(which curl)
+    local CURL=$(type -p curl)
 
-    if [ "x$CURL" = "x" ]; then
+    if [ -e "$CURL" ]
+    then
+        echo "[curl] exists at [$CURL] -- not installing!"
+    else
         doit sudo $INSTALL curl
     fi
 
 }
 
-# install stow
-install_stow() {
-    local _stow=$(which stow)
-
-    if [ "x$_stow" = "x" ]; then
-        doit sudo $INSTALL stow
-    fi
-}
-
 _install() {
     local _exe=$1
 
-    local _path=$(which $_exe)
+    local _path=$(type -p $_exe)
 
-    if [ -e $_path ]
+    if [ -e "$_path" ]
     then
         echo "[$_exe] exists at [$_path] -- not installing!"
     else
-        doit sudo $INSTALL $_exe
+        doit nix-env -i $_exe
     fi
 }
 
@@ -146,6 +135,21 @@ _stow() {
     fi
 }
 
+install_nix() {
+    url="https://nixos.org/nix/install"
+
+    if [ "file" = $(type -t nix) ]
+    then
+        echo "[$(type -p nix)] already exists! skipping"
+        return
+    fi
+
+    require_util curl "download things, like for installing nix"
+    res=$(curl $url | sh)
+    echo $res
+
+}
+
 WORKDIR=`dirname $0`
 CWD=`pwd`
 cd $WORKDIR
@@ -154,27 +158,46 @@ WORKDIR=`pwd`
 _SYSTEM=$(getsystem)
 INSTALL=$(setup)
 
-doit git submodule update --init --recursive
+submodule() {
+    doit git submodule update --init --recursive
+}
 
-echo "found install to be [$INSTALL]"
 
-_install "stow"
+runrun() {
+    install_curl
+    install_nix
+    _install stow
 
-install_sqlite
-_install curl
-_install vim
-_install tmux
+    _install sqlite3
+    _install vim
+    _install tmux
 
-link_file $WORKDIR/i3/config "$HOME/.config/i3/config"
-link_file $WORKDIR/vim/vim "$HOME/.vim"
-link_file $WORKDIR/vim/vimrc "$HOME/.vimrc"
-link_file $WORKDIR/stow/stowrc "$HOME/.stowrc"
-link_file $WORKDIR/tmux/tmux.conf "$HOME/.tmux.conf"
-link_file $WORKDIR/bash/bashrc "$HOME/.bashrc"
-link_file $WORKDIR/bash/bash_aliases "$HOME/.bash_aliases"
-link_file $WORKDIR/bash/bash_completion "$HOME/.bash_completion"
-link_file $WORKDIR/bash/bash_completion.d "$HOME/.bash_completion.d"
-link_file $WORKDIR/bash/bash_functions "$HOME/.bash_functions"
-link_file $WORKDIR/bash/bash_functions.d "$HOME/.bash_functions.d"
-link_file $WORKDIR/bash/dircolors "$HOME/.dircolors"
-link_file $WORKDIR/X/Xresources "$HOME/.Xresources"
+    link_file $WORKDIR/i3/config "$HOME/.config/i3/config"
+    link_file $WORKDIR/vim/vim "$HOME/.vim"
+    link_file $WORKDIR/vim/vimrc "$HOME/.vimrc"
+    link_file $WORKDIR/stow/stowrc "$HOME/.stowrc"
+    link_file $WORKDIR/tmux/tmux.conf "$HOME/.tmux.conf"
+    link_file $WORKDIR/bash/bashrc "$HOME/.bashrc"
+    link_file $WORKDIR/bash/bash_aliases "$HOME/.bash_aliases"
+    link_file $WORKDIR/bash/bash_completion "$HOME/.bash_completion"
+    link_file $WORKDIR/bash/bash_completion.d "$HOME/.bash_completion.d"
+    link_file $WORKDIR/bash/bash_functions "$HOME/.bash_functions"
+    link_file $WORKDIR/bash/bash_functions.d "$HOME/.bash_functions.d"
+    link_file $WORKDIR/bash/dircolors "$HOME/.dircolors"
+    link_file $WORKDIR/X/Xresources "$HOME/.Xresources"
+#    link_file $WORKDIR/direnv/direnvrc "$HOME/.direnvrc"
+}
+
+if [ "x$1" = "x" ]
+then
+    echo "Usage: $0 install"
+elif [ "x$1" = "xinstall" ]
+then
+    submodule
+    echo "found install to be [$INSTALL]"
+    runrun
+    echo "finished!"
+else
+    echo "trying to run whatever you passed. danger!!"
+    $1
+fi
